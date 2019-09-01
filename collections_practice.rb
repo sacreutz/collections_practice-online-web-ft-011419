ceb420e6f69cd1f139fd2943131796a5def5b810
Sophie Creutz <sophie.creutz@flatironschool.com>
2019-09-01 18:22:30 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,20 @@
+def sort_array_asc(array)
+  array.sort 
+end 
 
+def sort_array_desc(array)
+  array.sort{|a,b| b <=> a}
+end 
+
+def sort_array_char_count(array) 
+  array.sort{|a, b| a.length <=> b.length }
+end 
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
+
+def reverse_array(array)
+  array.sort{|a,b| b <=> a }
+end 
